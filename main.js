d5da293c3d6339f1e9a4ce7bbd2088467d7f09d7
jura339 <155677210+jura339@users.noreply.github.com>
2025-03-13 13:02:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,103 +1,29 @@
-import * as THREE from './three.js/build/three.module.js';
-import { OrbitControls } from './three.js/src/extras/OrbitControls.js';
-import { GLTFLoader } from './three.js/src/loaders/GLTFLoader.js';
-function main(){
-//camera
-    	const canvas = document.querySelector( '#c' );
-	  const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+//-------------------------------------------------------
+//parallax efekt
+//-------------------------------------------------------
+let para1 = document.getElementById('para1');
+let para2 = document.getElementById('para2');
+let para3 = document.getElementById('para3');
+let para4 = document.getElementById('para4');
+let para5 = document.getElementById('para5');
+let para6 = document.getElementById('para6');
+let map = document.getElementById('Mapa');
+//var h = window.innerHeight;
+//map.style.top = h + 'px';
 
-	  const fov = 45;
-	  const aspect = 2; // the canvas default
-	  const near = 0.1;
-	  const far = 100;
-	  const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	  camera.position.set( 0, 10, 20 );    camera.position.z = 2;
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color( 'cyan' );
+document.addEventListener('scroll', function () {
+    console.log(para1.style.height)
+    let value = window.scrollY;
+    para1.style.top = (value * -0.05) + 'px';
+    para2.style.top = (value * -0.15) + 'px';
+    para3.style.top = (value * -0.5) + 'px';
+    para4.style.top = (value * -0.9) + 'px';
+    para5.style.top = (value * -1.3) + 'px';
+    para6.style.top = (value * -1.6) + 'px';
+    map.style.top = (value * -3) + 'px';
+   // console.log(map.style.top);
+    });
 
-//box
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-
-    const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    renderer.render(scene, camera);
-//light
-    const color = 0xFFFFFF;
-    const intensity = 3;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
-    
-    const controls = new OrbitControls(camera, renderer.domElement);
-
-//instances
-  //floor
-    {
-      const planeSize = 40;
-  
-      const loader = new THREE.TextureLoader();
-      const texture = loader.load( 'https://threejs.org/manual/examples/resources/images/checker.png' );
-      texture.colorSpace = THREE.SRGBColorSpace;
-      texture.wrapS = THREE.RepeatWrapping;
-      texture.wrapT = THREE.RepeatWrapping;
-      texture.magFilter = THREE.NearestFilter;
-      const repeats = planeSize / 2;
-      texture.repeat.set( repeats, repeats );
-  
-      const planeGeo = new THREE.PlaneGeometry( planeSize, planeSize );
-      const planeMat = new THREE.MeshPhongMaterial( {
-        map: texture,
-        side: THREE.DoubleSide,
-      } );
-      const mesh = new THREE.Mesh( planeGeo, planeMat );
-      mesh.rotation.x = Math.PI * - .5;
-      scene.add( mesh );
-  
-    }
-    //map
-    {
-    const gltfLoader = new GLTFLoader();
-		gltfLoader.load( './webmap.gltf', ( gltf ) => {
-		const root = gltf.scene;
-		scene.add( root )}
-  );
-  };
-//canvas
-    function resizeRendererToDisplaySize(renderer) {
-        const canvas = renderer.domElement;
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-        const needResize = canvas.width !== width || canvas.height !== height;
-        if (needResize) {
-          renderer.setSize(width, height, false);
-        }
-        return needResize;
-      }
-
-
-    function render(time) {
-        time *= 0.001;  // convert time to seconds
-
-        if (resizeRendererToDisplaySize(renderer)) {
-          const canvas = renderer.domElement;
-          camera.aspect = canvas.clientWidth / canvas.clientHeight;
-          camera.updateProjectionMatrix();
-        }
-        
-        cube.rotation.x = time;
-        cube.rotation.y = time;
-       
-        renderer.render(scene, camera);
-       
-        requestAnimationFrame(render);
-      }
-      requestAnimationFrame(render);
-
-      
-}
-
-main();
+//-------------------------------------------------------
+//zmena obrazku
+//-------------------------------------------------------
